1308ea6f03738f3b53aff7f84480bba384642ee3
Sai Sujan <saisujan2707@gmail.com>
2026-04-28 10:25:21 +0530

    Fixed CORS for production

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002'],
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'https://dev-board-taupe.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
